21fcc2a2ae04c83763058f84090e4cba8bae2f95
Sadik Hasan <sadik.hasan@dynamicmalloc.xyz>
2023-08-28 20:25:34 +0600

    updated py

--- calculateDistance.py
+++ calculateDistance.py
@@ -1,8 +1,8 @@
 import requests
 
 # Replace with your actual coordinates
-start_coords = "90.399452,23.777176"  # San Francisco
-end_coords = "91.83168,22.3384"    # Los Angeles
+start_coords = "90.399452,23.777176"  # Dhaka
+end_coords = "91.83168,22.3384"    # Chittagong
 
 url = f"http://router.project-osrm.org/route/v1/car/{start_coords};{end_coords}"
 response = requests.get(url)
